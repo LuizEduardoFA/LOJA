bc92c9758cecbfdea1505f603640d64d31f946b6
LuizEduardoFA <165804409+LuizEduardoFA@users.noreply.github.com>
2025-02-25 14:21:11 -0300

    bsl

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ document.addEventListener('DOMContentLoaded', function(){
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte += 0.1;
+        tamanhoAtualFonte += 0.5;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
 
     diminuiFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte -= 0.1;
+        tamanhoAtualFonte -= 0.5;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
