3cde4adf3071d43ae12e67782c2637cf5e2d2ddd
LuizEduardoFA <165804409+LuizEduardoFA@users.noreply.github.com>
2025-03-18 13:21:16 -0300

    gbhfdjksl

---
+++ script.js
@@ -0,0 +1,21 @@
+const increaseFontButton = document.getElementById('increase-font');
+const decreaseFontButton = document.getElementById('decrease-font');
+const paragraphs = document.querySelectorAll('p');
+
+let currentFontSize = 16; // Tamanho de fonte inicial
+
+increaseFontButton.addEventListener('click', () => {
+  currentFontSize += 2;
+  updateFontSize();
+});
+
+decreaseFontButton.addEventListener('click', () => {
+  currentFontSize -= 2;
+  updateFontSize();
+});
+
+function updateFontSize() {
+  paragraphs.forEach(paragraph => {
+    paragraph.style.fontSize = `${currentFontSize}px`;
+  });
+}
