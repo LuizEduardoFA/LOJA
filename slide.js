c227ddbc8fcf6a401fb44fd7e24eee11983ccd9d
LuizEduardoFA <165804409+LuizEduardoFA@users.noreply.github.com>
2025-06-16 21:12:02 +0000

    ljhbgvfc

--- slide.js
+++ slide.js
@@ -1,1 +1,17 @@
-var radio = document.querySelector()
+var radio = document.querySelector( '.btnmanual')
+var cont = 1 
+
+document.getElementById('radio1').checked = true 
+
+setInterval(() => (
+    proximaImg()
+), 5000)
+
+function  proximaImg(){
+    cont++
+    if (cont > 3){
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true 
+}
